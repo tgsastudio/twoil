b019f74e99eced983ce5a2c1a9f8a8c0e612d67e
Simon <tgsastudio@gmail.com>
2015-04-15 18:24:13 +0800

    Remove demo files

--- protected/config/main.php
+++ protected/config/main.php
@@ -26,21 +26,14 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
+
 		'db'=>array(
-			'connectionString' => 'sqlite:protected/data/blog.db',
-			'tablePrefix' => 'tbl_',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=blog',
+			'connectionString' => 'mysql:host=127.0.0.1;dbname=twoil',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'tgsago123',
 			'charset' => 'utf8',
-			'tablePrefix' => 'tbl_',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
